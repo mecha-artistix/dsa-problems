a34bca9f03cc4fe18d13401cf592a0fddeeea949
Huzaifa Bin Abdul Qayyum <mecha.artistix@gmail.com>
2023-08-09 23:44:58 +0500

    ,

--- course-schedule.js
+++ course-schedule.js
@@ -36,4 +36,45 @@ var canFinish = function (numCourses, prerequisites) {
   }
   return true;
 };
-console.log(canFinish(numCourses, preReq));
+// console.log(canFinish(numCourses, preReq));
+
+// -> Using Topological Approach where we have indegree array that with keys(indeces = value/vertex) and values = to number of connected verteces
+
+var canFinishTopological = function (numCourses, prerequisites) {
+  const inDegree = new Array(numCourses).fill(0);
+  const adjList = inDegree.map(() => []);
+
+  for (let i = 0; i < prerequisites.length; i++) {
+    const pair = prerequisites[i];
+    inDegree[pair[0]]++;
+    adjList[pair[1]].push(pair[0]);
+  }
+
+  const stack = [];
+
+  for (let i = 0; i < inDegree.length; i++) {
+    if (inDegree[i] === 0) {
+      stack.push(i);
+    }
+  }
+
+  let count = 0;
+
+  while (stack.length) {
+    const current = stack.pop();
+    count++;
+
+    const adjacent = adjList[current];
+
+    for (let i = 0; i < adjacent.length; i++) {
+      const next = adjacent[i];
+      inDegree[next]--;
+      if (inDegree[next] === 0) {
+        stack.push(next);
+      }
+    }
+  }
+
+  return count === numCourses;
+};
+console.log(canFinishTopological(numCourses, preReq));
