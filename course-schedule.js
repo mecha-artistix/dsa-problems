b992df2f14f15daae35049f253cd544c3a28d916
Huzaifa Bin Abdul Qayyum <mecha.artistix@gmail.com>
2023-08-09 22:38:55 +0500

    .

--- course-schedule.js
+++ course-schedule.js
@@ -9,15 +9,32 @@ const preReq = [
   [4, 5],
 ];
 var canFinish = function (numCourses, prerequisites) {
-  const adjList = prerequisites.map(() => []);
-
+  debugger;
+  const adjList = new Array(numCourses).fill(0).map(() => []);
   for (let i = 0; i < prerequisites.length; i++) {
     const coursePair = prerequisites[i];
-    let adjListIdx = coursePair[1];
-    adjList[adjListIdx].push(coursePair[0]);
+    adjList[coursePair[1]].push(coursePair[0]);
   }
-  return adjList;
+
+  //- loop over the total size of n using bfs
+
+  for (let v = 0; v < numCourses; v++) {
+    const queue = [];
+    const seen = {};
+    for (let i = 0; i < adjList[v].length; i++) {
+      queue.push(adjList[v][i]);
+    }
+    while (queue.length) {
+      const current = queue.shift();
+      seen[current] = true;
+      if (current == v) return false;
+      const adj = adjList[current];
+      for (let i = 0; i < adj.length; i++) {
+        const next = adj[i];
+        if (!seen[next]) queue.push(next);
+      }
+    }
+  }
+  return true;
 };
 console.log(canFinish(numCourses, preReq));
-const adj = new Array(6).fill(9).map(() => [9]);
-console.log(adj);
